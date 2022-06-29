29141fcd7c18cf2a9823ce542921a2a9881305cb
ionite34 <dev@ionite.io>
2022-06-29 17:14:49 -0400

    Verbose error for 'specialty' spelling

--- qualifier/tests.py
+++ qualifier/tests.py
@@ -27,7 +27,32 @@ def create_request(
         receive: Callable[[], Awaitable[object]] = _receive,
         send: Callable[[object], Awaitable[Any]] = _send
 ) -> Request:
-    return Request(MappingProxyType(scope), receive, send)
+    """
+    Create a request object with the given scope and receive/send functions.
+    Raises an error with help message if 'specialty' is accessed.
+    """
+    _err = RuntimeError(
+        "You may be using the wrong spelling for 'speciality'"
+        "; The correct key name is 'speciality', not 'specialty'."
+    )
+
+    # Dict wrapper
+    class DictWrapper(dict):
+        def __getitem__(self, key):
+            if key == "specialty":
+                raise _err
+            return super().__getitem__(key)
+
+        def get(self, key, default=None):
+            if key == "specialty":
+                raise _err
+            return super().get(key, default)
+
+    # Added specialty spelling warning if `speciality` is used
+    if 'speciality' in scope:
+        scope = DictWrapper(scope)
+
+    return Request(scope, receive, send)
 
 
 def wrap_receive_mock(id_: str, mock: AsyncMock) -> Callable[[], Awaitable[object]]:
